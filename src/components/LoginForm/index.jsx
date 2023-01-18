812542b2959c0f8bd10414a4722f0371de0e67a5
codeawy <codeawy@gmail.com>
2023-01-18 23:18:46 +0200

    form inputs

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,19 +1,68 @@
+import { Fragment, useState } from "react";
+import loginFormInputs from "../../list/loginFormInputs";
 import "./LoginForm.scss";
 
+// TODO Add select element
 const LoginForm = () => {
+  const [userInfo, setUserInfo] = useState({
+    password: "",
+    username: "Naga",
+    address: "",
+    email: "",
+    cars: "",
+  });
+
+  /** ------- HANDLER ------- */
+  const loginFormHandler = event => {
+    const { name, value } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  /** ------- RENDER ------- */
+  const renderFormInputs = loginFormInputs.map(({ type, name, label, required }, idx) => {
+    return idx === 2 ? (
+      <Fragment key={idx}>
+        <select name="cars" id="cars" value={userInfo.cars} onChange={loginFormHandler}>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+        <div className="input">
+          <input
+            className="input-field"
+            type={type}
+            name={name}
+            value={userInfo[name]}
+            onChange={loginFormHandler}
+            required={required}
+          />
+          <label className="input-label">{label}</label>
+        </div>
+      </Fragment>
+    ) : (
+      <div className="input" key={idx}>
+        <input
+          className="input-field"
+          type={type}
+          name={name}
+          value={userInfo[name]}
+          onChange={loginFormHandler}
+          required={required}
+        />
+        <label className="input-label">{label}</label>
+      </div>
+    );
+  });
+
   return (
     <div className="container">
       <div className="card">
         <div className="card-image" />
+
         <form className="card-form" autoComplete="off">
-          <div className="input">
-            <input type="email" className="input-field" required />
-            <label className="input-label">Email</label>
-          </div>
-          <div className="input">
-            <input type="password" className="input-field" required />
-            <label className="input-label">Password</label>
-          </div>
+          {renderFormInputs}
+
           <div className="action">
             <button className="action-button">Get started</button>
           </div>
@@ -29,3 +78,7 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
+// * Controlled Component
+// *  - Value
+// *  - onChange => event
---
+++ src/list/loginFormInputs.js
@@ -0,0 +1,8 @@
+const loginFormInputs = [
+  { label: "Username", type: "text", name: "username", required: true },
+  { label: "Email", type: "email", name: "email", required: true },
+  { label: "Password", type: "password", name: "password", required: true },
+  { label: "Address", type: "text", name: "address", required: false },
+];
+
+export default loginFormInputs;
